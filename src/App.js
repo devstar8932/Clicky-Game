4b36874a8eb3ce7d08349bbd90cfe959345c37ab
Kim Kaufman <kimgomizzou@hotmail.com>
2017-12-08 18:55:26 -0600

    fix folder structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Section from "./components/Section";
-
-import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
     <Section />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Container/Container.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Container.css";
+
+const Container = () => (
+
+
+
+
+
+export default Navbar;

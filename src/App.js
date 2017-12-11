c35308c0112a91c60f10e64a5be62fb8ef5e02dc
Kim Kaufman <kimgomizzou@hotmail.com>
2017-12-11 16:46:00 -0600

    adding pictures

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-// import Section from "./components/Container";
+import Container from "./components/Container";
 // import Footer from "./components/Footer";
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
+    <div>
+	    <Container />
+    	<Container />
+    	<Container />
+    </div>	
+
+
   </div>
 );
 
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Container.css";
 
-// const Container = () => (
+const Container = props => (
+ <div className="card">
+    <div className="img-container">
+      <img src={props.image} />
+    </div>
+  </div>
+);
 
-
-
-
-
-// export default Container;
+export default Container;
